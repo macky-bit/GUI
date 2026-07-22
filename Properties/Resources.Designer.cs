efd8717591a93eb6df8397f87180de4918836990
Mark Engels Manzano <marklubianmanzamo@gmail.com>
2026-07-22 22:32:38 +0800

    fix

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace WindowsFormsApp1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap dmmsu_logo__1_ {
-            get {
-                object obj = ResourceManager.GetObject("dmmsu_logo (1)", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap dmmsu1 {
             get {
                 object obj = ResourceManager.GetObject("dmmsu1", resourceCulture);
